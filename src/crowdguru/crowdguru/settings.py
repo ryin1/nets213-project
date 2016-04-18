fb6d5288e3a11a9876f9815c71b877b264c3be4e
Raymond Yin <ryin@seas.upenn.edu>
2016-04-18 14:25:34 -0400

    config

--- src/crowdguru/crowdguru/settings.py
+++ src/crowdguru/crowdguru/settings.py
@@ -143,9 +143,3 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
