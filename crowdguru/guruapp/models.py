90274608f1ff21722b9f273438885c2750fd2bbe
Raymond Yin <ryin@seas.upenn.edu>
2016-04-18 13:43:22 -0400

    app before heroku

---
+++ crowdguru/guruapp/models.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+class Achievement(models.Model):
+    description = models.CharField(max_length=300)
+
+class Item(models.Model):
+    name = models.CharField(max_length=300)
+    category = models.ForeignKey(Category)
+
+# class User(AbstractUser):
+#     '''
+#     Custom User model
+#     '''
+#     achievements = models.ManyToManyField(Achievement)
+
+#     # profile information
+#     birthday = models.DateField(default=timezone.now)
+#     firstname = models.CharField(max_length=30)
+#     lastname = models.CharField(max_length=30)
+#     bio = models.TextField()
+#     categories = models.ManyToManyField(Category)
+
+#     def __str__(self):
+#         return self.username
+
+class Question(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    created_at = models.DateTimeField(default=timezone.now)
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    preferences = models.CharField(max_length=1000) # JSON list of strings
+    tags = models.CharField(max_length=500) # JSON list of strings
+    is_resolved = models.BooleanField()
+    spam_counts = models.IntegerField()
+
+class Recommendation(models.Model):
+    question = models.ForeignKey(Question)
+    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User)
+    recommendation = models.TextField()
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    spam_counts = models.IntegerField()
+    is_star = models.BooleanField()
